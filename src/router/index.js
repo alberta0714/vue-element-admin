5212cd48c0a75e433111004b6bbf63032a1d560f
孙占超 <sunzhanchao@igetcool.com>
2021-11-16 19:26:09 +0800

    添加忽略文件，及注释

--- src/router/index.js
+++ src/router/index.js
@@ -388,7 +388,7 @@ export const asyncRoutes = [
 ]
 
 const createRouter = () => new Router({
-  // mode: 'history', // require service support
+  // mode: 'history', // require service support, {browserHistory,hashHistory(默认#)}
   scrollBehavior: () => ({ y: 0 }),
   routes: constantRoutes
 })
