d13c8f851fdf921546a6ad1b04fb5a94f2722009
孙占超 <sunzhanchao@igetcool.com>
2021-11-26 13:19:13 +0800

    调整脚本

--- buildprod.sh
+++ buildprod.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
+# 设置你的分支
+CURRENT_BRANCH=optimize/v202111-trace
+
 npm run build:prod
 rm -rf "D:\wks_igetcool\architecture\igetcool-data-query\igetcool-data-query-sw-server\src\main\resources\public"
 mkdir "D:\wks_igetcool\architecture\igetcool-data-query\igetcool-data-query-sw-server\src\main\resources\public"
@@ -16,9 +19,9 @@ git add *
 git commit -a -m "【sw前端调整】"
 git push
 git checkout dev
-git merge optimize/v202111-trace
+git merge ${CURRENT_BRANCH}
 git push
-git checkout optimize/v202111-trace
+git checkout ${CURRENT_BRANCH}
 git status
 
 
